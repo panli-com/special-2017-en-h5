7873943884369ce9cab184626033ee36578ff677
zanjs <root@zanjs.com>
2017-08-31 14:03:04 +0800

    再把新建的分支删掉： git branch -d dev :dash:

--- 0526/src/js/main.js
+++ 0526/src/js/main.js
@@ -2,14 +2,36 @@
 
 // var notyf = new Notyf();
 
-// ;
-// (function () {
-//     // Create an instance of Notyf
-//     // var notyf = new Notyf(); 
+// var classVal = document.getElementById("id").getAttribute("class");
 
-//     window.onload = function () {
+// //删除的话
+// classVal = classVal.replace("someClassName","");
+// document.getElementById("id").setAttribute("class",classVal );
+
+// //添加的话
+// classVal = classVal.concat(" someClassName");
+// document.getElementById("id").setAttribute("class",classVal );
+
+// //替换的话
+// classVal = classVal.replace("someClassName","otherClassName");
+// document.getElementById("id").setAttribute("class",classVal );
+
+;
+(function() {
+  // Create an instance of Notyf
+  // var notyf = new Notyf(); 
 
 
+})()
 
-//     }
-// })()
+// function showBtb() {
+//   var endTime = +new Date("2017/09/07 10:0:0")
+//   var nowTime = +new Date()
+
+//   if (nowTime > endTime) {
+//     document.getElementById("btn1").setAttribute("class", "")
+//   } else {
+//     document.getElementById("btn2").setAttribute("class", "")
+//   }
+// }
+// window.onload = showBtb
